773ad89e44d64cd82c5f2f37e0f17b2b60a8fa7e
Honey Sebastian <honeysebastian2000@gmail.com>
2024-04-24 22:44:26 +0530

    New files created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Landing from './Pages/Landing'
+import Home from './Pages/Home'
+import History from './Pages/History'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
   
 
   return (
     <>
+    <Header/>
+    <Routes>
+      <Route element={<Landing/>} path='/'/>
+      <Route element={<Home/>} path='/home'/>
+      <Route element={<History/>} path='/history'/>
+    </Routes>
+    <Footer/>
       
       
     </>
